5fa4fbde188a7d07aafa0db185a72d1b2ed644ce
samutamm <samutamm@gmail.com>
2016-01-12 09:54:38 +0100

    Push to heroku-with toolbelt

--- src/main/java/com/samutamm/newsreader/spark/Server.java
+++ src/main/java/com/samutamm/newsreader/spark/Server.java
@@ -5,10 +5,18 @@ import static spark.Spark.*;
 
 public class Server {
     public void run() {
+        String port = System.getenv("PORT");
+        System.out.println(port);
+        if(port != null) {
+            System.out.println(port);
+            port(Integer.parseInt(port));
+        }
+        
         get("/hello", (req, res) -> "Hello World");
+        get("/", (req, res) -> "Bolobolo");
         awaitInitialization();
     }
-    
+
     public void closeServer() {
         stop();
     }
